d4af67b168e3287e19687719d3b6e51c546d773c
Sri Harsha Boda <bodasriharsha@gmail.com>
2017-07-11 21:46:22 -0700

    adding support to write to HBase

--- spark-streaming/src/main/java/driver/Test.java
+++ spark-streaming/src/main/java/driver/Test.java
@@ -1,47 +1,61 @@
 package driver;
 
-import kafka.serializer.StringDecoder;
-import org.apache.spark.SparkConf;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.streaming.Duration;
-import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaPairInputDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import org.apache.spark.streaming.kafka.KafkaUtils;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 import scala.Tuple2;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-/**
- * Created by cloudera on 6/27/17.
- */
 public class Test {
-    public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("Log Analyzer");
-        conf.setMaster("local[2]");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaStreamingContext ssc = new JavaStreamingContext(sc, new Duration(10000));
-        Map<String,String> kafkaParams = new HashMap<String,String>();
-        kafkaParams.put("bootstrap.servers","localhost:9092");
-        kafkaParams.put("zookeeper.connect","localhost:2181");
-        Set<String> topics = new HashSet<>();
-        topics.add("test");
-        JavaPairInputDStream<String, String> directKafkaStream = KafkaUtils.createDirectStream(ssc, String.class, String.class, StringDecoder.class, StringDecoder.class, kafkaParams, topics);
-        JavaDStream finalStream = directKafkaStream.map(Tuple2::_2);
-        finalStream.transform(new Function<JavaRDD<String>, JavaRDD<String>>() {
+    public static void main(String[] args) throws Exception {
+// create connection with HBase
+       /* Configuration config = null;
+        try {
+            config = HBaseConfiguration.create();
+            config.set("hbase.zookeeper.quorum", "127.0.0.1");
+            config.set("hbase.zookeeper.property.clientPort", "2181");
+            config.set("hbase.master", "127.0.0.1:60000");
+            HBaseAdmin.checkHBaseAvailable(config);
+            System.out.println("HBase is running!");
+        } catch (MasterNotRunningException e) {
+            System.out.println("HBase is not running!");
+            System.exit(1);
+        } catch (Exception ce) {
+            ce.printStackTrace();
+        }
+        config.set(TableInputFormat.INPUT_TABLE, "tableName");
+        // new Hadoop API configuration
+        Job newAPIJobConfiguration1 = Job.getInstance(config);
+        newAPIJobConfiguration1.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, "tableName");
+        newAPIJobConfiguration1.setOutputFormatClass(org.apache.hadoop.hbase.mapreduce.TableOutputFormat.class);
+        // create Key, Value pair to store in HBase
+        JavaPairRDD<ImmutableBytesWritable, Put> hbasePuts = javaRDD.mapToPair(new PairFunction<Row, ImmutableBytesWritable, Put>() {
             @Override
-            public JavaRDD<String> call(JavaRDD<String> stringJavaRDD) throws Exception {
-                stringJavaRDD.take(10);
-                return null;
+            public Tuple2<ImmutableBytesWritable, Put> call(Row row) throws Exception {
+                Put put = new Put(Bytes.toBytes(row.getString(0)));
+                put.add(Bytes.toBytes("columFamily"), Bytes.toBytes("columnQualifier1"), Bytes.toBytes(row.getString(1)));
+                put.add(Bytes.toBytes("columFamily"), Bytes.toBytes("columnQualifier2"), Bytes.toBytes(row.getString(2)));
+                return new Tuple2<ImmutableBytesWritable, Put>(new ImmutableBytesWritable(), put);
             }
         });
-        finalStream.print();
-        ssc.start();
-        ssc.awaitTermination();
+        // save to HBase- Spark built-in API method
+        hbasePuts.saveAsNewAPIHadoopDataset(newAPIJobConfiguration1.getConfiguration());*/
+
     }
+
+
 }
